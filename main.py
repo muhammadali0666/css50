4ac2c86a1754eadc5f27c57e847db4f1d127881d
muhammadali0666 <muhammadalishuhratjonov50@gmail.com>
2024-01-10 06:40:57 +0500

    css50 course added

---
+++ main.py
@@ -0,0 +1,96 @@
+# a = 10.2
+# a= "salom"
+
+# print(a, type(a))
+
+# a = True
+
+# print(a, type(a))
+# b = False
+
+# print(b, type(b))
+
+# son = int(10.88)
+# fson = float(son)
+# print(fson, type(fson))
+
+# a = int(10)
+# b = float(12.3)
+
+# print(a+b, type(b))
+
+# a = "a"
+# b = 11
+# c = True
+
+# print("a = {0} b = {2} c = {1}".format(a,b,c))
+
+# text = "salom dunyo"
+
+# print(*text, sep="-")
+# print(text[-1])
+# print(text[::2])
+
+# text = """salom "bolla" qalesila"""
+
+# print(text)
+
+# a = input("sonni kiriting")
+
+# print(a, type(a))
+
+# print(1>2 or 2<3)
+
+# text = "alom"
+
+# if "s" in text:
+#   print("bor")
+# else: 
+#   print("yo'q")
+
+# for i in range(1,10,2):
+#   print(i)
+
+# x = ["salom"]
+
+# print(type(x))
+
+# a = slice(2) 
+# def stutter(word):
+# 	x =	(word[a] + "... ") * 2 + word+"?"
+# 	return x
+
+# print(stutter("salom"))
+
+# def dis(price, discount):
+#   divide = price // 100
+#   res = divide * discount
+#   return res
+# print(dis(200, 25))
+
+# num = 1234
+# reversed_num = 0
+
+# while num != 0:
+#     digit = num % 10
+#     reversed_num = reversed_num * 10 + digit
+#     num //= 10
+
+# print("Reversed Number: " + str(reversed_num))
+
+# import random
+
+# a = ["cards", "cars"]
+
+# random.shuffle(a)
+
+# for i in a:
+#   print(i)
+
+# import statistics
+
+
+# print(statistics.mean([1,15]))
+
+# import sys 
+	
